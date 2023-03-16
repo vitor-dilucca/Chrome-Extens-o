c5b9eb31e356e394d32bc3b717f5cb604a62fa58
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-16 06:15:08 -0300

    extensão chrome finalizado funcionando apropriadamente

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-// 5:16:30
-
+myLeads = []
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 const divEx = document.querySelector("#container-exercicio")
 const h3El = document.querySelector("#h3-el")
+const clearBtn = document.querySelector("#clear-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.querySelector("#tab-btn")
 
-myLeads = []
-// myLeads=JSON.parse(myLeads)
-// myLeads.push("www.imprisoningme.com")
-// myLeads=JSON.stringify(myLeads)
-// console.log(myLeads)
+oldLeads = []
 
-// localStorage.setItem("myLeads","www.darkness.com")
-// console.log(localStorage.getItem("myLeads"))
-// localStorage.clear()
-// console.log(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({active:true, currentWindow:true}, tabs=>{
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
 
 inputBtn.addEventListener("click", function () {
   //insere o q for escrito no input dentro do array myLeads
   myLeads.push(inputEl.value)
   inputEl.value = ''
   //transforma o array myLeads em uma string "myLeads" e insere dentro do localStorage
-  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   //printa o conteudo do array myLeads
-  renderLeads()
+  render(myLeads)
   //da console.log da string "myLeads"
   console.log(localStorage.getItem("myLeads"))
 })
 
-function renderLeads() {
+clearBtn.addEventListener("dblclick", () => {
+  localStorage.clear()
+  myLeads = []
+  ulEl.innerHTML = ''
+})
+
+function render(leads) {
   let listItems = ''
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a target="blank" href="${myLeads[i]}">
-        ${myLeads[i]}
+      <a target="blank" href="${leads[i]}">
+        ${leads[i]}
       </a>
     </li>
     `
   }
   ulEl.innerHTML = listItems
 }
-
-// divEx.innerHTML = '<button>Compre Já</button>'
-// h3El.innerHTML = '<p>Obrigado por comprar volte sempre</p>'
