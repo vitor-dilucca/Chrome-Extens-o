8e33567ffd1e5141f0284b8df1b02ad6015dbb32
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-16 02:09:31 -0300

    inserindo input no myLeads, transformando oa array em string para poder inserir no localStorage e inserindo, inserindo no dom

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // 5:16:30
-myLeads = []
+
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 const divEx = document.querySelector("#container-exercicio")
 const h3El = document.querySelector("#h3-el")
 
+myLeads = []
+// myLeads=JSON.parse(myLeads)
+// myLeads.push("www.imprisoningme.com")
+// myLeads=JSON.stringify(myLeads)
+// console.log(myLeads)
+
+// localStorage.setItem("myLeads","www.darkness.com")
+// console.log(localStorage.getItem("myLeads"))
+// localStorage.clear()
+// console.log(localStorage.getItem("myLeads"))
+
 inputBtn.addEventListener("click", function () {
+  //insere o q for escrito no input dentro do array myLeads
   myLeads.push(inputEl.value)
   inputEl.value = ''
+  //transforma o array myLeads em uma string "myLeads" e insere dentro do localStorage
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  //printa o conteudo do array myLeads
   renderLeads()
+  //da console.log da string "myLeads"
+  console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
